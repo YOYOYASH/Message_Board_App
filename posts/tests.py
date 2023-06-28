efd57655a896278b276ddefe16ec50a9195560df
YOYOYASH <ym911837@student.nitw.ac.in>
2023-06-28 23:38:25 +0530

    tests added

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Post
+
+
+class PostModelTest(TestCase):
+    def setUp(self):
+        Post.objects.create(text='Just a test')
+
+    def test_text_content(self):
+        post = Post.objects.get(id=1)
+        expected_object = f'{post.text}'
+        self.assertEquals(expected_object, 'Just a test')
+
+
+class HomePageViewTest(TestCase):
+    def setUp(self):
+        Post.objects.create(text='Just a test')
+
+    def test_url_exists(self):
+        resp = self.client.get('/')
+        self.assertEquals(resp.status_code, 200)
+
+    def test_view_url(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEquals(resp.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'home.html')
